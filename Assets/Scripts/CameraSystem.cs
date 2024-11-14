1bea924e66b113d8559f32f828367c6d3d8f0e8b
Ytanbius <ytalogsouza@gmail.com>
2024-11-14 20:00:02 -0300

    Update CameraSystem.cs

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
 {
-    
+    public GameObject triggerCam;
+    public GameObject targetDestroy;
     public GameObject curCam;
-    
+
+    private void Update()
+    {
+        if (curCam == triggerCam)
+        {
+
+            targetDestroy.SetActive(false);
+
+        }
+    }
 }
